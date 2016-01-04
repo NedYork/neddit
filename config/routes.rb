58113f383faf44a67bfa19f543c158b7e83b86ba
Ned&Frankie <Ned@Frankie.io>
2016-01-04 14:33:29 -0500

    Create buttons back to sub. More views work.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  root to: "subs#index"
+
   resources :users, only: [:new, :create, :show]
   resource :session, only: [:new, :create, :destroy]
   resources :subs, except: [:destroy] do
     resources :posts, only: [:new]
   end
   resources :posts, except: [:index, :new]
+
 end
